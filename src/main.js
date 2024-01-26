cedad0e209204320d398abe74769db1dbbbafaa7
sirwilliander <101196370+sirwilliander@users.noreply.github.com>
2024-01-26 10:13:56 +0100

    Deletion of comments

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import App from './App.svelte';
 new App({
 	target: document.body,
 	props: {
-		// What's your name?
 		name: 'Máté Simon',
-		// In the following fiels you can either give a single string, 
-		// or an array of bullet points.
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
