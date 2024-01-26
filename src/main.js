4e2caf33cbf3f332f8411568a6bacfcef516d4e9
sirwilliander <101196370+sirwilliander@users.noreply.github.com>
2024-01-26 07:00:27 +0100

    Personalize main.js

--- src/main.js
+++ src/main.js
@@ -4,25 +4,26 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Máté Simon',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Maintenance of the pipelines. If something is red --> make it green'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Jenkins',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'Get to know more about Jenkins and github actions'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'wall climbing', 
+			'boardgaming', 
+			'videogaming',
+			'playing football'
 		]
 	}
 });
